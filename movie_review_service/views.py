baea58b7a86050ccb3b76b3c0c3d63b77ee1be52
ReyLegar <38353663+ReyLegar@users.noreply.github.com>
2023-03-07 09:18:48 +0300

    Update views.py

--- movie_review_service/views.py
+++ movie_review_service/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, redirect
 import joblib
 from .forms import ReviewForm
 from django.http import HttpResponseRedirect
+import os
+from django.conf import settings
 
 
 
@@ -21,22 +23,23 @@ def input_page(request):
     if request.method == 'POST':
         request_text = request.POST
         text_review = request_text['text']
-        vectorizer = joblib.load('vectorizer.joblib')
-        clf = joblib.load('model_svm.pkl')
+        vec_path = os.path.join(settings.STATICFILES_DIRS[0], 'vectorizer.joblib')
+        vectorizer = joblib.load(vec_path)
+        pkl = os.path.join(settings.STATICFILES_DIRS[0], 'model_svm.pkl')
+        clf = joblib.load(pkl)
         vectorizer_text = vectorizer.transform([text_review])
         pred = clf.predict(vectorizer_text)
         if pred > 5:
             emo = "Positive"
-        else: 
+        else:
             emo = "Negative"
         rew = Review.objects.create(text=text_review, rating=pred[0], emotion=emo)
-        rew.save() 
+        rew.save()
         return redirect(reviews)
     return render(request, 'input_page.html')
 
 def reviews(request):
-    reviews = Review.objects.all()
+    reviews = Review.objects.all().order_by('-id')
     return render(request, 'review.html', {'reviews': reviews})
 
-  
-    
+
